f2c661d2ea49e4976f6ab251c3bc7e15d7490c00
bnl75 <diegoartu@hotmail.com>
2019-01-16 00:06:03 -0600

    Index completo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.carousel').carousel({dist:0, shift:30});
+	$('.fixed-action-btn').floatingActionButton();
+
+  });
